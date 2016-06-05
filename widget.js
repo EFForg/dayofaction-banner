d62fd94a2849d86546079d8856602f14ee2a2274
Sina Khanifar <sina.khanifar@gmail.com>
2016-06-04 22:32:12 -0700

    Cleanup (and magnification) of widget.js

--- widget.js
+++ widget.js
@@ -9,7 +9,7 @@
                               
   =============================================================================
 
-  Support the NoGlobalWarrant campaign by installing this banner on your site.
+  Support the NoGlobalWarrants.org campaign by installing this banner on your site.
 
   <sina@eff.org> for support
 
@@ -45,21 +45,9 @@
 var _banner_config = (typeof banner_config  !== 'undefined') ? banner_config  : {};
 
 (function (window, widgetConfig) {
-  var JSON;JSON||(JSON={});
-
-  // Race against time hack to cache locations, need stringify to put in cookie cross browser
-
-  // JSON2 - See http://www.JSON.org/js.html - Public Domain
-  (function(){function k(a){return a<10?"0"+a:a}function o(a){p.lastIndex=0;return p.test(a)?'"'+a.replace(p,function(a){var c=r[a];return typeof c==="string"?c:"\\u"+("0000"+a.charCodeAt(0).toString(16)).slice(-4)})+'"':'"'+a+'"'}function l(a,j){var c,d,h,m,g=e,f,b=j[a];b&&typeof b==="object"&&typeof b.toJSON==="function"&&(b=b.toJSON(a));typeof i==="function"&&(b=i.call(j,a,b));switch(typeof b){case "string":return o(b);case "number":return isFinite(b)?String(b):"null";case "boolean":case "null":return String(b);case "object":if(!b)return"null";
-  e+=n;f=[];if(Object.prototype.toString.apply(b)==="[object Array]"){m=b.length;for(c=0;c<m;c+=1)f[c]=l(c,b)||"null";h=f.length===0?"[]":e?"[\n"+e+f.join(",\n"+e)+"\n"+g+"]":"["+f.join(",")+"]";e=g;return h}if(i&&typeof i==="object"){m=i.length;for(c=0;c<m;c+=1)typeof i[c]==="string"&&(d=i[c],(h=l(d,b))&&f.push(o(d)+(e?": ":":")+h))}else for(d in b)Object.prototype.hasOwnProperty.call(b,d)&&(h=l(d,b))&&f.push(o(d)+(e?": ":":")+h);h=f.length===0?"{}":e?"{\n"+e+f.join(",\n"+e)+"\n"+g+"}":"{"+f.join(",")+
-  "}";e=g;return h}}if(typeof Date.prototype.toJSON!=="function")Date.prototype.toJSON=function(){return isFinite(this.valueOf())?this.getUTCFullYear()+"-"+k(this.getUTCMonth()+1)+"-"+k(this.getUTCDate())+"T"+k(this.getUTCHours())+":"+k(this.getUTCMinutes())+":"+k(this.getUTCSeconds())+"Z":null},String.prototype.toJSON=Number.prototype.toJSON=Boolean.prototype.toJSON=function(){return this.valueOf()};var q=/[\u0000\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g,
-  p=/[\\\"\x00-\x1f\x7f-\x9f\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g,e,n,r={"\u0008":"\\b","\t":"\\t","\n":"\\n","\u000c":"\\f","\r":"\\r",'"':'\\"',"\\":"\\\\"},i;if(typeof JSON.stringify!=="function")JSON.stringify=function(a,j,c){var d;n=e="";if(typeof c==="number")for(d=0;d<c;d+=1)n+=" ";else typeof c==="string"&&(n=c);if((i=j)&&typeof j!=="function"&&(typeof j!=="object"||typeof j.length!=="number"))throw Error("JSON.stringify");return l("",
-  {"":a})};if(typeof JSON.parse!=="function")JSON.parse=function(a,e){function c(a,d){var g,f,b=a[d];if(b&&typeof b==="object")for(g in b)Object.prototype.hasOwnProperty.call(b,g)&&(f=c(b,g),f!==void 0?b[g]=f:delete b[g]);return e.call(a,d,b)}var d,a=String(a);q.lastIndex=0;q.test(a)&&(a=a.replace(q,function(a){return"\\u"+("0000"+a.charCodeAt(0).toString(16)).slice(-4)}));if(/^[\],:{}\s]*$/.test(a.replace(/\\(?:["\\\/bfnrt]|u[0-9a-fA-F]{4})/g,"@").replace(/"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g,
-  "]").replace(/(?:^|:|,)(?:\s*\[)+/g,"")))return d=eval("("+a+")"),typeof e==="function"?c({"":d},""):d;throw new SyntaxError("JSON.parse");}})();
 
   // Do configuration
   widgetConfig.show_style = widgetConfig.show_style || 'banner';
-  widgetConfig.greeting = widgetConfig.greeting || 'Dear Internet';
   widgetConfig.debug = widgetConfig.debug || false;
   widgetConfig.localAssets = widgetConfig.localAssets || false;
   widgetConfig.callOnly = widgetConfig.callOnly || false;
@@ -67,7 +55,6 @@ var _banner_config = (typeof banner_config  !== 'undefined') ? banner_config  : 
   widgetConfig.disableDate = widgetConfig.disableDate || false;
   widgetConfig.campaign = widgetConfig.campaign || 'noglobalwarrants';
   widgetConfig.cookieTimeout = widgetConfig.cookieTimeout || 172800;
-  widgetConfig.overrideLocation = widgetConfig.overrideLocation || false;
 
   function debug() {
     if (widgetConfig.debug) {
@@ -107,7 +94,7 @@ var _banner_config = (typeof banner_config  !== 'undefined') ? banner_config  : 
        if((that.fullSize && x < 767) || (!that.fullSize && x > 767)) {
           if(window.tdwfbResizeCallback) {
             clearTimeout(window.tdwfbResizeCallback);
-          };
+          }
           window.tdwfbResizeCallback = setTimeout(function () {
               that.show({location: window.tdwfbLocation, widgetConfig: widgetConfig});
               windowWidth = x;
@@ -143,37 +130,14 @@ var _banner_config = (typeof banner_config  !== 'undefined') ? banner_config  : 
 
       // This used to check the date by getting it from a server, but now just uses the device's local date.
 
-      window.tdwfbDateCallBackFailSafe = setTimeout(function () {
+      window.dateCallBackFailSafe = setTimeout(function () {
         var today = new Date();
         if (today.getDate() === activeCampaign.runDate.day && today.getMonth() === activeCampaign.runDate.month - 1 && today.getFullYear() === activeCampaign.runDate.year) {
-          debug('yay worked');
           callback({activeToday: true});
         } else {
           callback({activeToday: false});
         }
       }, 1);
-    },
-    location: function (callback) {      // This location check is only used if disableLocation is not set to true.
-      debug('location()');
-
-      window.tdwfbParseLocation = function (data) {
-        debug('tdwfbParseLocation()', data);
-
-        callback(data);
-      };
-      var locationCookie = getCookie(activeCampaign.locationCookieName);
-      if(locationCookie === null) {
-        var script = document.createElement('script');
-        script.src = 'https://geoip.taskforce.is/?callback=tdwfbParseLocation';
-        document.getElementsByTagName('head')[0].appendChild(script);
-
-        window.tdwfbLocationCallBackFailSafe = setTimeout(function () {
-          // Set location to US and pass to callback
-          callback({country: {iso_code: 'US'}});
-        }, 5000);
-      } else {
-        callback(JSON.parse(locationCookie));
-      }
     }
   };
 
@@ -181,7 +145,6 @@ var _banner_config = (typeof banner_config  !== 'undefined') ? banner_config  : 
   var campaign = {
     noglobalwarrants: {
       cookieName: 'noglobalwarrants_hasseen',
-      locationCookieName: 'noglobalwarrants_locationb',
       runDate: {
         day: 21,
         month: 6, // Use natural month, e.g. January = 1
@@ -224,7 +187,6 @@ var _banner_config = (typeof banner_config  !== 'undefined') ? banner_config  : 
         }
       },
       minimized: false,
-      usesLocation: false,
       show: function (options) {
         debug('show()', options);
 
@@ -299,8 +261,6 @@ var _banner_config = (typeof banner_config  !== 'undefined') ? banner_config  : 
         document.body.appendChild(campaignSpacer);
         document.body.appendChild(campaignContainer);
 
-        var isoCode = options.location && options.location.country &&
-          options.location.country.iso_code || 'unknown';
         var firstTime = true;
 
         if (cookie !== null) {
@@ -317,9 +277,7 @@ var _banner_config = (typeof banner_config  !== 'undefined') ? banner_config  : 
         iframe.style.cssText = style.iframe;
 
         var us = iframe.src = ASSET_URL + activeCampaign.config.show_style +
-          '.html?firstTime=' + firstTime + '&callOnly=' +
-          widgetConfig.callOnly + '&iso=' + isoCode + '&greeting=' +
-          widgetConfig.greeting;
+          '.html?firstTime=' + firstTime;
 
         iframeContainer.appendChild(iframe);
 
@@ -401,36 +359,10 @@ var _banner_config = (typeof banner_config  !== 'undefined') ? banner_config  : 
         checks.correctDate(function (response) {
           debug('correctDate() callback', response);
 
-          clearTimeout(window.tdwfbDateCallBackFailSafe);
+          clearTimeout(window.dateCallBackFailSafe);
 
           if (response && (response.activeToday ||
               widgetConfig.disableDate || widgetConfig.debug)) {
-
-            if (activeCampaign.usesLocation) {
-              checks.location(function (location) {
-                debug('location() callback', location);
-
-                window.tdwfbLocation = location;
-
-                clearTimeout(window.tdwfbLocationCallBackFailSafe);
-
-                var cookieLoc = {
-                  country: {
-                    iso_code: location.country.iso_code
-                  }
-                }
-                setCookie(activeCampaign.locationCookieName, JSON.stringify(cookieLoc), widgetConfig.cookieTimeout );
-
-                activeCampaign.show({
-                  location: location,
-                  widgetConfig: widgetConfig
-                });
-
-                setResizeListener();
-              });
-            }
-            else {
-              clearTimeout(window.tdwfbLocationCallBackFailSafe);
 
               activeCampaign.show({
                 location: null,
@@ -438,7 +370,6 @@ var _banner_config = (typeof banner_config  !== 'undefined') ? banner_config  : 
               });
 
               setResizeListener();
-            }
           }
         });
       }
