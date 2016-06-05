bbefd8641aae7de9e77236a6bac84dac4a74b73a
Sina Khanifar <sina.khanifar@gmail.com>
2016-06-04 22:00:37 -0700

    Email action working

--- banner_content/js/main.js
+++ banner_content/js/main.js
@@ -6,4 +6,8 @@ $(document).ready( function () {
         window.open('./petition.html', "_blank", "width=500,height=650,scrollbars=no");
         return false;
     });
+    $('#action_us').on('click', function(ev) {
+        window.open('./email.html', "_blank", "width=500,height=625,scrollbars=no");
+        return false;
+    });
 });
