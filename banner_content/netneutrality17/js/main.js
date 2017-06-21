463abfd9ab3e863a8c8d38aae03ea9544993bced
Schatzkin <schatzkin@eff.org>
2017-06-20 17:46:07 -0700

    added timer, hide/show, buttons for closing/not closing

--- banner_content/netneutrality17/js/main.js
+++ banner_content/netneutrality17/js/main.js
@@ -1,14 +1,19 @@
-$(document).ready( function () {
-  var listener = function() {
-    document.removeEventListener('mouseover', listener);
-    document.getElementsByTagName('body')[0].classList.add('frame2');
-  };
-  document.addEventListener("mouseover", listener);
-  setTimeout(listener, 5000);
-  $('#frame1').click(function (e) {
-    e.preventDefault();
-    $('#frame1').css("display","none");
-    $('#frame2').css("display","block");
+$(window).on('load',function(){
+  $('#dayofaction').modal('show');
+  function dayHideShow() {
+    $('#frame1').hide();
+    $('#frame2').show("slow");
+    $('.real').css("display","block");
+    $('.fake').css("display","none");
+  }
+  $('#frame1').on('mouseover', function(event) {
+    timer = window.setTimeout(function() {
+      dayHideShow();
+    }, 4000);
   });
 
+  $('#frame1').click(function (e) {
+    e.defaultPrevented;
+    dayHideShow();
+  });
 });
